9668cfdaac846f722de57865ee17247030463339
Chris <smallchris19@gmail.com>
2022-11-26 20:24:34 -0700

    Implemented Login Page

--- ClubBAISTDev/Pages/Login.cshtml.cs
+++ ClubBAISTDev/Pages/Login.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClubBAISTDev.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,33 @@ namespace ClubBAISTDev.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public int MemberIdField { get; set; }
+
+        [BindProperty]
+        public string PasswordField { get; set; }
+
+        public string Message { get; set; }
+
+        CBS RequestDirector = new();
+
+        bool Success;
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            Success = RequestDirector.Login(MemberIdField, PasswordField);
+
+            if (Success)
+            {
+                Message = "Member Logged In! Redirecting...";
+            } else
+            {
+                Message = "Incorrect Member ID or Password";
+            }
         }
     }
 }
