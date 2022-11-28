0bc0bad90cba1903319c70b09fff03d970ced0bc
Chris <smallchris19@gmail.com>
2022-11-28 16:30:15 -0700

    Added validation for dailyteesheet

--- ClubBAISTDev/Pages/CreateTeeTime.cshtml.cs
+++ ClubBAISTDev/Pages/CreateTeeTime.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClubBAISTDev.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,9 +13,32 @@ namespace ClubBAISTDev.Pages
         [BindProperty]
         public DateTime TeeSheetDateField { get; set; }
 
+        public DailyTeeSheet TeeSheet { get; set; }
+
+        [BindProperty]
+        public string Submit { get; set; }
+
+        public string Message { get; set; }
+
+        public CBS RequestDirector = new();
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            switch(Submit)
+            {
+                case "SearchTeeSheet":
+
+                    TeeSheet = RequestDirector.GetDailyTeeSheet(TeeSheetDateField);
+                    break;
+
+                case "RequestTeeTime":
+                    break;
+            }
         }
     }
 }
